39ec6e1546a89e82c4a3ce11b3a052ac5a4dd492
NMCannon <nmichaelcannon@gmail.com>
2021-11-22 20:04:40 +0000

    added gravity and player jump ability

--- Games Engines Project/Assets/PlayerMovement.cs
+++ Games Engines Project/Assets/PlayerMovement.cs
@@ -7,7 +7,17 @@ public class PlayerMovement : MonoBehaviour
 
     public CharacterController controller;
 
-    public float speed = 12f;
+    public float speed = 18f;
+    public float gravity = -22f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +27,35 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Return true if object touching ground layer
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        // If player is touching ground and not moving vertically
+        if(isGrounded && velocity.y <0)
+        {
+            // Reset the velocity to keep player grounded
+            velocity.y = -2f;
+        }
+
+        // Get wasd input
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        // Vector for player movement
         Vector3 move = transform.right * x + transform.forward * z;
 
+        // Move player with character controller
         controller.Move(move * speed * Time.deltaTime);
+
+        // If space is pressed and player is touching ground layer
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            // Lift player up by amount needed to reach jumpHeight
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Apply gravity to player
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
--- Games Engines Project/Assets/mouselook.cs
+++ Games Engines Project/Assets/mouselook.cs
@@ -6,9 +6,7 @@ public class mouselook : MonoBehaviour
 {
 
     public float mouseSensitivity = 100f;
-
     public Transform playerBody;
-
     float xRotation = 0f;
 
     // Start is called before the first frame update
@@ -21,13 +19,19 @@ public class mouselook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Get mouse input for x axis
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        // Get mouse input for y axis
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+
         xRotation -= mouseY;
+        // Clamp rotation before -90 and 90 degrees so player can't over-rotate
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
+        // Rotate player on y axis
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        // Rotate player on x axis
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
